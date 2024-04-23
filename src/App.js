9ce904daed39414723d9de16afa00f6e5ce43445
Success Bright <bs3webdevelop@gmail.com>
2024-04-23 13:40:42 +0100

    update all

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import "./App.css";
-import Navbar from "./navbar";
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <div className="container mx-auto">
-        {/* Your content goes here */}
-        <h1 className="text-3xl mt-8">Welcome to My React App!</h1>
-        <p className="text-lg">This is a test page with a simple navbar.</p>
-      </div>
-      ReactDom.render(
-        <h1 className="header">This is JSX</h1>, document.getElementById("root")
-      )
-    </div>
-  );
-
-}
+const App = () => <div>Start here!</div>
 
 export default App;
